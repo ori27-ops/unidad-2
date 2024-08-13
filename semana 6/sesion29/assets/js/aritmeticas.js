0d675d3200a7373abff728bd47153ca4070fe0d3
ori27_ops <pironaoriana@gmail.com>
2024-08-12 19:41:32 -0500

    sesion29

---
+++ semana 6/sesion29/assets/js/aritmeticas.js
@@ -0,0 +1,12 @@
+//Función para sumar dos números
+function sumar(a, b) {
+    return a + b
+}
+
+function restar(a, b) {
+    return a - b
+}
+export {
+    sumar, 
+    restar
+};
---
+++ semana 6/sesion29/assets/js/script.js
@@ -0,0 +1,6 @@
+import {sumar, restar } from "./aritmeticas.js"
+
+//Usamos la funcion sumar
+console.log(sumar(15, 34));
+//Restar
+console.log(restar(15, 76));
